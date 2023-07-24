f0a12579a1056bb71b16d0c57032156d28170799
jacobdholloway <jholloway1342@sdsu.edu>
2023-07-24 12:58:20 -0700

    Modified countMul to be more efficient

--- two/testmulthreads.c
+++ two/testmulthreads.c
@@ -1,67 +1,53 @@
 /* This file is to test your program#2.
- * 
+ *
  */
 
-#include <stdio.h> 
-#include <dirent.h> 
+#include <stdio.h>
+#include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h> 
-#include <stdbool.h> 
+#include <unistd.h>
+#include <stdbool.h>
 #include <time.h>
 #include "count.h"
-
-
 
 /*
  * Print the frequencies of alphabetical characters stored in array: charfreq[] to output screen in the format as:
  * letter -> frequency (one letter a line)
  * Input: charfreq - array having the frequency of each alphabet letter
-          size     - the total number of alphabet letters 
+		  size     - the total number of alphabet letters
  * example:
- * a -> 20 
- * b -> 30 
+ * a -> 20
+ * b -> 30
  */
- 
+
 void displayalphabetfreq(long charfreq[], int size)
-{	
-	 
-	for(int i = 0; i < size; i++)
-	  {
-	  	printf("%c -> %d\n", (char)(i+97), charfreq[i]);
-	  }
+{
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%c -> %d\n", (char)(i + 97), charfreq[i]);
+	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	
-	time_t currentTime;
-	time(&currentTime);
-    	char *timeString = ctime(&currentTime);
-	
+	if (argc < 2)
+	{
+		printf("Please enter 2 arguments only eg.\"./testmulthreads #_of__threads!!\"\n");
+		return 1;
+	}
 
-    if (argc < 2) {
-        printf("Please enter 2 arguments only eg.\"./testmulthreads #_of__threads!!\"\n");
-        return 1;
-    }
+	int num_threads = atoi(argv[1]);
 
-    	int num_threads = atoi(argv[1]);
-	  
-	 char *path = "/home/cs/zhengli/cssc1729/ProgrammingAssignments/two/data";           // the data *.txt files are under this folder
-	 char *filetowrite = "/home/cs/zhengli/cssc1729/ProgrammingAssignments/two/result/filetowrite.txt";    // the frequency of all alphabetical letters will be written in this file
-	  
-	  long alphabetfreq[ALPHABETSIZE] = {0}; // array to store the frequency of each alphablet letter, which should be alway up-to-date;
-	  
-	  
-	  alphabetcountmulthreads(path, filetowrite, alphabetfreq, num_threads);  // process the data files using mutiple threads
-	  
-	  printf("\nTask was completed on : %s \n", timeString);
-	  printf("The results are counted as follows : \n\n");
-	  
-	  
+	char *path = "../two/data"; // the data *.txt files are under this folder
+	char *filetowrite = "../two/result/filetowrite.txt"; // the frequency of all alphabetical letters will be written in this file
 
-	  displayalphabetfreq(alphabetfreq, ALPHABETSIZE);  // print the frequency stored in the array to output screen
+	long alphabetfreq[ALPHABETSIZE] = {0}; // array to store the frequency of each alphablet letter, which should be alway up-to-date;
 
+	alphabetcountmulthreads(path, filetowrite, alphabetfreq, num_threads); // process the data files using mutiple threads
+
+	displayalphabetfreq(alphabetfreq, ALPHABETSIZE); // print the frequency stored in the array to output screen
 }
-
