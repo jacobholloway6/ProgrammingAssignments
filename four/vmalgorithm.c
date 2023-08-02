f7489197903c9b27a39856e9a6425f339313c256
jacobdholloway <jholloway1342@sdsu.edu>
2023-08-02 09:01:40 -0700

    Added information in Readme and comments to vmalgorithm

--- four/vmalgorithm.c
+++ four/vmalgorithm.c
@@ -17,12 +17,18 @@ void generateAccessPattern()
 {
    int i;
    srand(time(0));
-   accessPattern = (int *)malloc(sizeof(int) * AccessPatternLength);
-   printf("The randomized Access Pattern: ");
+   accessPattern = (int *)malloc(sizeof(int) * AccessPatternLength); // Allocates memory dynamically for the array & each element is the size of an integer
+
+   if(&malloc == NULL){
+      printf("Memory was not allocated successfully. \n"); // Validates that memory allocation was successful
+      exit -1;
+   }
+
+   printf("The randomized Access Pattern: "); // Provides terminal output of the random access pattern that was generated using the srand(time) function
    for (i = 0; i < AccessPatternLength; i++)
    {
-      accessPattern[i] = rand() % ReferenceSZ;
-      printf("%d ", accessPattern[i]);
+      accessPattern[i] = rand() % ReferenceSZ; // For each index, generate a random number which represents a page to be accessed
+      printf("%d ", accessPattern[i]); // Random number is stored at accessPattern
    }
    
    printf("\n");
@@ -34,6 +40,9 @@ void generateAccessPattern()
 void initializePageFrame()
 {
    int i;
+
+   // Allocates memory dynamically for the array 'PageFrameList' in 'memory' of size 'FrameNR'
+   // This represents the memory frames to hold the pages
    memory.PageFrameList = (int *)malloc(sizeof(int) * FrameNR); // dynamic allocated FrameNR frames to be used in memory
    memory.nextToReplaced = 0;                                   // indicate the new frame to be replaced as 0
    for (i = 0; i < FrameNR; i++)
@@ -49,6 +58,8 @@ void printPageFrame()
    int i;
    for (i = 0; i < FrameNR; i++)
    {
+      // Sets the value of each frame to -1, indicating that the frame is initially unused
+      // This assumes that valid page numbers are non-negative
       printf("%2d ", memory.PageFrameList[i]);
    }
    printf("\n");
@@ -74,7 +85,7 @@ void printAccessPattern()
 int FIFO()
 {
 
-   // TODO: fill the code to impelement FIFO replacement algorithm
+   
    int i;
    int pageFault = 0;
    int pageIndex;
@@ -82,6 +93,7 @@ int FIFO()
    int j;
    int frameNum;
 
+   // Ensures that the number of frames does not exceed the number of virtual pages
    // be sure that we never go beyond the number of virtual page
    if (FrameNR > ReferenceSZ)
    {
@@ -92,13 +104,15 @@ int FIFO()
       frameNum = FrameNR;
    }
 
-   // iterate over the pattern
+   // iterate over access pattern which is the sequence of page accesses
    for (i = 0; i < AccessPatternLength; i++)
    {
       // get page number
       page = accessPattern[i];
 
-      // try to find a physical page which this page is in at the moment
+      // Attempt to find the page in memory (within page frame list)
+      // If found, pageIndex will hold the index at which the page was found
+      // If not found, pageIndex will remain as -1
       pageIndex = -1;
       for (j = 0; j < frameNum; j++)
       {
@@ -126,7 +140,8 @@ int FIFO()
       }
       printf("\n");
    }
-
+   
+   // Return the total number of page faults that have occurred. 
    return pageFault;
 }
 
@@ -136,8 +151,6 @@ int FIFO()
 
 int LRU()
 {
-
-   // TODO: fill the code to impelement LRU replacement algorithm
 
    int i;
    int pageFault = 0;
@@ -149,6 +162,8 @@ int LRU()
    int logicalClock = 0;
    int maxVal;
 
+   
+// Ensures that the number of frames do not exceed the number of virtual pages
    if (FrameNR > ReferenceSZ)
    {
       frameNum = ReferenceSZ;
@@ -162,6 +177,9 @@ int LRU()
     * Add a register to every page frame - contain the last time that
     * the page in that frame was accessed
     */
+   
+   // Allocate an array of registers, one for each frame
+   // Each register keeps track of the "logical time" the page in the corresponding frame was last accessed
    registers = (int *)calloc(frameNum, sizeof(int));
    for (i = 0; i < frameNum; i++)
    {
@@ -178,10 +196,10 @@ int LRU()
    {
       // Each time a page is referenced, update its register
       logicalClock++;
-      // get page number
+      // get page number from the access pattern
       page = accessPattern[i];
 
-      // try to find a physical page which this page is in at the moment
+      // Try to find a physical page in memory in the page list
       pageIndex = -1;
       for (j = 0; j < frameNum; j++)
       {
@@ -198,7 +216,8 @@ int LRU()
       {
          pageFault++;
 
-         // find the least recent used slot
+         // Find the frame with the oldest (smallest) logical time
+         // This will be the least recently used frame
          memory.nextToReplaced = 0;
 
          // the life of page cannot be longer than AccessPatternLength
@@ -211,14 +230,14 @@ int LRU()
                maxVal = registers[j];
             }
          }
-         // update the slot
+         // Replaces the least recently used fram with the new page frame
          memory.PageFrameList[memory.nextToReplaced] = page;
          // update its logical clock
          registers[memory.nextToReplaced] = logicalClock;
       }
       else
       {
-         // update the logical time of this slot
+         // update the logical time of this slot if the page was found in memory 
          registers[pageIndex] = logicalClock;
       }
       // print the physical memory out
@@ -229,7 +248,7 @@ int LRU()
       printf("\n");
    }
 
-   // deallocate memory on heap
+   // deallocates memory on heap
    free(registers);
    return pageFault;
 }
